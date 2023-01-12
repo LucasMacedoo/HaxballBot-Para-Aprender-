797f8f9e3bdc06b5f42c5eab9e5d4fa2c0e2c656
KyotoHax <41812186+KyotoHax@users.noreply.github.com>
2023-01-12 17:40:35 -0300

    Update Criando Sala - 1.js

--- Formas de Criar Sala/Criando Sala - 1.js
+++ Formas de Criar Sala/Criando Sala - 1.js
@@ -8,5 +8,5 @@ var sala = HBInit ({
 	public: true, //True para sala pública, false para sala privada 
 	geo: {code: 'br', lat: -46.4515, lon: -23.5344}, //Bandeira do país e localização com latitude e longitude
 	token: "seu token aqui"/*, //Token para sala carregar sem recaptcha
-	noPlayer: true */ //Caso não queira o jogador BOT na sala, utilize true
+	noPlayer: true */ //Caso não queira o jogador BOT na sala, utilize true, caso contrário, não é necessário mencionar, mas pode utilizar false
 });
