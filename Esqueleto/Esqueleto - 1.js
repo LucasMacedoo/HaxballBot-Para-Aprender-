adce2a0078b2eeecb561e3f1616d3cd8d5db08fa
Lucas Macêdo <41812186+LucasMacedoo@users.noreply.github.com>
2024-10-05 18:01:01 -0300

    Update Esqueleto - 1.js
    Atualização do Headless Haxball

--- Esqueleto/Esqueleto - 1.js
+++ Esqueleto/Esqueleto - 1.js
@@ -109,3 +109,8 @@ sala.onRoomLink = function(url) {
 sala.onKickRateLimitSet = function(min, rate, burst, jogadorDeu) {
 	
 }
+
+//Após alterar as configurações de bloqueio de equipe
+sala.onTeamsLockChange = function(bloqueio, jogadorDeu) {
+	
+}
