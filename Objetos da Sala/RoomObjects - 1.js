918cc2c6770b8cf818d7242e17c67b79159aac80
Lucas Macêdo <41812186+LucasMacedoo@users.noreply.github.com>
2024-10-07 14:27:13 -0300

    Update RoomObjects - 1.js

--- Objetos da Sala/RoomObjects - 1.js
+++ Objetos da Sala/RoomObjects - 1.js
@@ -111,6 +111,27 @@ sala.onPlayerChat = function(jogador,mensagem) {
 		//sala.clearBan precisa somente de um número inteiro que representa o ID do jogador
 		sala.clearBan(1); //Desbane especificamente um usuário pelo id dele, a utilização do número 1 é só um exemplo
 	}
+	
+	//Utilização do clearBans
+	if(mensagem == "!desbanAll") {
+		//sala.clearBans não possui nenhum atributo
+		sala.clearBans(); //Desbane todos os usuários que estão banidos da sala
+	}
+	
+	//Utilização do setDefaultStadium
+	//ESTÁDIOS DEFAULT: Classic, Easy, Small, Big, Rounded, Hockey, Big Hockey, Big Easy, Big Rounded e Huge
+	if(mensagem == "!trocarDefault") {
+		//sala.setDefaultStadium precisa somente de uma string que representa o nome do estádio default
+		sala.setDefaultStadium("Hockey");
+	}
+	
+	//Utilização do setCustomStadium
+	if(mensagem == "!trocarCustom") {
+		//sala.setCustomStadium precisa somente de uma variável ou string que representa o estádio customizado
+		//veja abaixo um exemplo de uma string de um pequeno estádio customizado
+		//você encontra esses arquivos no haxmaps ou poderá criar um usando haxpuck
+		sala.setCustomStadium(estadio);
+	}
 }
 
 //Após o jogador chutar a bola
@@ -192,3 +213,117 @@ sala.onKickRateLimitSet = function(min, rate, burst, jogadorDeu) {
 sala.onTeamsLockChange = function(bloqueio, jogadorDeu) {
 	
 }
+
+//Somente para exemplificar a troca de um estádio customizado
+var estadio = `
+{
+
+	"name" : "Small Futsal from HaxMaps",
+
+	"width" : 420,
+
+	"height" : 200,
+
+	"spawnDistance" : 130,
+
+	"bg" : { "type" : "hockey", "width" : 320, "height" : 130, "kickOffRadius" : 70, "cornerRadius" : 0 },
+
+	"vertexes" : [
+		/* 0 */ { "x" : -320, "y" : 130, "trait" : "ballArea" },
+		/* 1 */ { "x" : -320, "y" : 55, "trait" : "ballArea" },
+		/* 2 */ { "x" : -320, "y" : -55, "trait" : "ballArea" },
+		/* 3 */ { "x" : -320, "y" : -130, "trait" : "ballArea" },
+		/* 4 */ { "x" : 320, "y" : 130, "trait" : "ballArea" },
+		/* 5 */ { "x" : 320, "y" : 55, "trait" : "ballArea" },
+		/* 6 */ { "x" : 320, "y" : -55, "trait" : "ballArea" },
+		/* 7 */ { "x" : 320, "y" : -130, "trait" : "ballArea" },
+		
+		/* 8 */ { "x" : 0, "y" : 200, "trait" : "kickOffBarrier" },
+		/* 9 */ { "x" : 0, "y" : 70, "trait" : "kickOffBarrier" },
+		/* 10 */ { "x" : 0, "y" : -70, "trait" : "kickOffBarrier" },
+		/* 11 */ { "x" : 0, "y" : -200, "trait" : "kickOffBarrier" },
+		
+		/* 12 */ { "x" : -330, "y" : -55, "trait" : "goalNet" },
+		/* 13 */ { "x" : -350, "y" : -35, "trait" : "goalNet" },
+		/* 14 */ { "x" : -350, "y" : 35, "trait" : "goalNet" },
+		/* 15 */ { "x" : -330, "y" : 55, "trait" : "goalNet" },
+		/* 16 */ { "x" : 330, "y" : -55, "trait" : "goalNet" },
+		/* 17 */ { "x" : 350, "y" : -35, "trait" : "goalNet" },
+		/* 18 */ { "x" : 350, "y" : 35, "trait" : "goalNet" },
+		/* 19 */ { "x" : 330, "y" : 55, "trait" : "goalNet" }
+
+	],
+
+	"segments" : [
+		{ "v0" : 0, "v1" : 1, "trait" : "ballArea" },
+		{ "v0" : 2, "v1" : 3, "trait" : "ballArea" },
+		{ "v0" : 4, "v1" : 5, "trait" : "ballArea" },
+		{ "v0" : 6, "v1" : 7, "trait" : "ballArea" },
+		
+		{ "v0" : 12, "v1" : 13, "trait" : "goalNet", "curve" : -90 },
+		{ "v0" : 13, "v1" : 14, "trait" : "goalNet" },
+		{ "v0" : 14, "v1" : 15, "trait" : "goalNet", "curve" : -90 },
+		{ "v0" : 16, "v1" : 17, "trait" : "goalNet", "curve" : 90 },
+		{ "v0" : 17, "v1" : 18, "trait" : "goalNet" },
+		{ "v0" : 18, "v1" : 19, "trait" : "goalNet", "curve" : 90 },
+		
+		{ "v0" : 8, "v1" : 9, "trait" : "kickOffBarrier" },
+		{ "v0" : 9, "v1" : 10, "trait" : "kickOffBarrier", "curve" : 180, "cGroup" : ["blueKO" ] },
+		{ "v0" : 9, "v1" : 10, "trait" : "kickOffBarrier", "curve" : -180, "cGroup" : ["redKO" ] },
+		{ "v0" : 10, "v1" : 11, "trait" : "kickOffBarrier" }
+
+	],
+
+	"goals" : [
+		{ "p0" : [-320,55 ], "p1" : [-320,-55 ], "team" : "red" },
+		{ "p0" : [320,55 ], "p1" : [320,-55 ], "team" : "blue" }
+
+	],
+
+	"discs" : [
+		{ "pos" : [-320,55 ], "trait" : "goalPost", "color" : "FFCCCC" },
+		{ "pos" : [-320,-55 ], "trait" : "goalPost", "color" : "FFCCCC" },
+		{ "pos" : [320,55 ], "trait" : "goalPost", "color" : "CCCCFF" },
+		{ "pos" : [320,-55 ], "trait" : "goalPost", "color" : "CCCCFF" }
+
+	],
+
+	"planes" : [
+		{ "normal" : [0,1 ], "dist" : -130, "trait" : "ballArea" },
+		{ "normal" : [0,-1 ], "dist" : -130, "trait" : "ballArea" },
+		
+		{ "normal" : [0,1 ], "dist" : -200, "bCoef" : 0.1 },
+		{ "normal" : [0,-1 ], "dist" : -200, "bCoef" : 0.1 },
+		{ "normal" : [1,0 ], "dist" : -420, "bCoef" : 0.1 },
+		{ "normal" : [-1,0 ], "dist" : -420, "bCoef" : 0.1 }
+
+	],
+
+	"traits" : {
+		"ballArea" : { "vis" : false, "bCoef" : 1, "cMask" : ["ball" ] },
+		"goalPost" : { "radius" : 8, "invMass" : 0, "bCoef" : 0.5 },
+		"goalNet" : { "vis" : true, "bCoef" : 0.1, "cMask" : ["ball" ] },
+		"kickOffBarrier" : { "vis" : false, "bCoef" : 0.1, "cGroup" : ["redKO","blueKO" ], "cMask" : ["red","blue" ] }
+
+	},
+
+	"playerPhysics" : {
+		"kickStrength" : 7,
+		"kickingDamping" : 0.96,
+		"acceleration" : 0.1
+
+	},
+
+	"ballPhysics" : {
+		"radius" : 10,
+		"invMass" : 1,
+		"damping" : 0.96,
+		"color" : "FFFFFF",
+		"cMask" : [ "all"
+		],
+		"cGroup" : [ "ball"
+		]
+
+	}
+}
+`
