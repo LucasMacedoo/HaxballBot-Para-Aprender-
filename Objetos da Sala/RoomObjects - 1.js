50c80a6cdc873006c692a6e368b8a23a5c410e58
Lucas Macêdo <41812186+LucasMacedoo@users.noreply.github.com>
2024-10-07 13:59:59 -0300

    Update RoomObjects - 1.js

--- Objetos da Sala/RoomObjects - 1.js
+++ Objetos da Sala/RoomObjects - 1.js
@@ -17,8 +17,15 @@ var sala = HBInit ({
 
 //Ao entrar na sala
 sala.onPlayerJoin = function(jogador) {
+	//sala.sendChat precisa de uma string, e opcionalmente um id para especificar quem deve receber, null ou undefined todo mundo recebe
 	sala.sendChat("Olá, seja bem vindo a esta sala"); //Player BOT digita na sala o que está aqui
+	
+	//sala.setPlayerAdmin precisa de um id de um jogador e um true (para dar admin) ou false (para tirar admin)
 	sala.setPlayerAdmin(jogador.id, true); //Dá ou tira administrador para o id do usuário em questão
+	
+	//sala.sendAnnouncement precisa de uma string, um id de quem deve receber (null ou undefined todo mundo recebe), uma cor começando por 0x ,
+	//um estilo e um som
+	sala.sendAnnouncement("Vamos jogar?", null, 0x00BFFF, "bold", 1); //Uma mensagem personalizada é colocada no chat
 }
 
 //Ao sair da sala
@@ -35,6 +42,7 @@ sala.onTeamVictory = function(pontos) {
 sala.onPlayerChat = function(jogador,mensagem) {
 	//Utilização do setPlayerTeam
 	if(mensagem == "!azul") {
+		//sala.setPlayerTeam precisa de um id de algum jogador e de um id de time (0: spec, 1: red, 2: blue)
 		sala.setPlayerTeam(jogador.id, 2); //Modifica o jogador de time, no caso, vai pra equipe azul
 	} 
 	else if(mensagem == "!vermelho") {
@@ -46,6 +54,7 @@ sala.onPlayerChat = function(jogador,mensagem) {
 	
 	//Utilização do kickPlayer
 	if(mensagem == "!kick") {
+		//sala.kickPlayer precisa de um id de algum jogador, uma string que é o motivo e true (para banir) ou false (só kick normal)
 		sala.kickPlayer(jogador.id, "kick", false); //Expulsa o jogador da sala, neste caso ele poderá voltar
 	}
 	else if(mensagem == "!ban") {
@@ -54,17 +63,20 @@ sala.onPlayerChat = function(jogador,mensagem) {
 	
 	//Utilização do setScoreLimit
 	if(mensagem == "!score") {
+		//sala.setScoreLimit precisa somente de um número inteiro para representar a quantidade de gols limite de uma partida
 		sala.setScoreLimit(6); //Muda o placar limite da partida, 6 é um número qualquer inserido
 	}
 	
 	//Utilização do setTimeLimit
 	if(mensagem == "!time") {
+		//sala.setTimeLimit precisa somente de um número inteiro para representar o tempo limite de uma partida
 		sala.setTimeLimit(6); //Muda o tempo limite da partida, 6 é um número qualquer inserido
 	}
 	
 	//Utilização do setTeamsLock
 	//OBS: Administradores podem modificar as equipes da mesma forma, isto vale para usuários comuns
 	if(mensagem == "!bloquear") {
+		//sala.setTeamsLock precisa somente de um true (para bloquear mudança) ou false (para permitir mudança)
 		sala.setTeamsLock(true); //Modifica a configuração de mudança de equipe, neste caso, proibe mudanças
 	}
 	else if(mensagem == "!desbloquear") {
@@ -73,12 +85,24 @@ sala.onPlayerChat = function(jogador,mensagem) {
 	
 	//Utilização do startGame
 	if(mensagem == "!iniciar") {
+		//sala.startGame não possui nenhum atributo
 		sala.startGame(); //Inicia a partida (caso ainda não tenha iniciado)
 	}
 	
 	//Utilização do stopGame
 	if(mensagem == "!finalizar") {
+		//sala.stopGame não possui nenhum atributo
 		sala.stopGame(); //Finaliza a partida (caso ainda não esteja finalizado)
+	}
+	
+	//Utilização do pauseGame
+	//OBS: Só vale para partidas inicializadas
+	if(mensagem == "!pausar") {
+		//sala.pauseGame precisa somente de um true (para pausar) ou false (para despausar)
+		sala.pauseGame(true); //Modifica o estado de pausa da partida, neste caso, jogo pausado
+	}
+	else if(mensagem == "!despausar") {
+		sala.pauseGame(false); //Modifica o estado de pausa da partida, neste caso, jogo despausado
 	}
 }
 
